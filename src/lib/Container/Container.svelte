2e93ab053b2d37658b6969f73499e8606acabc2a
M B Parvez (Ron) <mbparvezme@gmail.com>
2023-11-07 20:27:35 +0600

    1.0.0-beta.52

--- src/lib/Container/Container.svelte
+++ src/lib/Container/Container.svelte
@@ -2,6 +2,6 @@
   import { twMerge } from "tailwind-merge"
 </script>
 
-<section class="theui-container {twMerge("relative w-full max-w-[var(--max-width)] mx-auto px-8 py-24 md:px-16", $$props?.class)}">
+<section class="theui-container {twMerge("relative w-full max-w-[var(--max-width)] mx-auto px-8 py-24", $$props?.class)}">
   <slot></slot>
 </section>
