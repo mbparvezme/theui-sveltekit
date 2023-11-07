40e649da6c343bf70c3aa917cde4410799c56703
M B Parvez (Ron) <mbparvezme@gmail.com>
2023-11-07 12:34:55 +0600

    1.0.0-beta.43

--- src/lib/Navbar/NavCollapse.svelte
+++ src/lib/Navbar/NavCollapse.svelte
@@ -11,11 +11,11 @@
   let resCls = "flex-col basis-full absolute left-0 right-0 top-full order-last lg:order-none shadow-lg px-8 py-4 "
   
   resCls += config.responsive == "xl"   ?
-              "xl-collapse xl:flex-row xl:items-center xl:basis-auto xl:static xl:left-auto xl:right-auto xl:top-auto xl:flex xl:overflow-visible xl:shadow-none xl:px-0 xl:py-0 xl:bg-transparent " :
+              "xl-collapse 2xl:flex-row 2xl:items-center 2xl:basis-auto 2xl:static 2xl:left-auto 2xl:right-auto 2xl:top-auto 2xl:flex 2xl:overflow-visible 2xl:shadow-none 2xl:px-0 2xl:py-0 2xl:bg-transparent " :
             config.responsive == "md"   ?
-              "md-collapse md:flex-row md:items-center md:basis-auto md:static md:left-auto md:right-auto md:top-auto md:flex md:overflow-visible md:shadow-none xl:px-0 md:py-0 md:bg-transparent " :
+              "md-collapse lg:flex-row lg:items-center lg:basis-auto lg:static lg:left-auto lg:right-auto lg:top-auto lg:flex lg:overflow-visible lg:shadow-none lg:py-0 lg:bg-transparent lg:px-0 " :
             config.responsive == "lg"   ?
-              "lg-collapse lg:flex-row lg:items-center lg:basis-auto lg:static lg:left-auto lg:right-auto lg:top-auto lg:flex lg:overflow-visible lg:shadow-none xl:px-0 lg:py-0 lg:bg-transparent " : ""
+              "lg-collapse xl:flex-row xl:items-center xl:basis-auto xl:static xl:left-auto xl:right-auto xl:top-auto xl:flex xl:overflow-visible xl:shadow-none xl:px-0 xl:py-0 xl:bg-transparent " : ""
 </script>
 
 <div
--- src/lib/Navbar/NavToggle.svelte
+++ src/lib/Navbar/NavToggle.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { getContext } from "svelte"
   import { NAV, Button, Svg } from "$lib"
-  import { twMerge } from "tailwind-merge";
+  import { twMerge } from "tailwind-merge"
   const { config, id, mobileNav } = getContext(NAV)
 
   let toggle = () => {
@@ -15,7 +15,7 @@
 </script>
 
 {#if config.responsive !== false}
-<Button on:click={()=>toggle()} class="nav-toggle {twMerge("bg-transparent", $$props.class)} {config.responsive == "xl" ? "xl:hidden" : config.responsive == "md" ? "md:hidden" : config.responsive == "lg" ? "lg:hidden" : "hidden"}" ariaLabel="Toggle nav bar" label="">
+<Button on:click={()=>toggle()} class="nav-toggle {twMerge("bg-transparent", $$props.class)} {config.responsive == "xl" ? "2xl:hidden" : config.responsive == "md" ? "lg:hidden" : config.responsive == "lg" ? "xl:hidden" : "hidden"}" ariaLabel="Toggle nav bar" label="">
   <slot name="menuIcon" slot="label">
     <Svg size={1.75} fill="none" viewBox="0 0 24 24" stroke-width="2" class=" stroke-gray-700 dark:stroke-gray-300">
       <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
