96589b9467ec146d37b40bac79d269c009cb4b3f
M B Parvez (Ron) <mbparvezme@gmail.com>
2023-11-21 08:57:42 +0600

    Update +page.svelte

--- src/routes/accordion/+page.svelte
+++ src/routes/accordion/+page.svelte
@@ -21,32 +21,32 @@
     A
     <AccordionItem>
         <span slot="title">This is a accordion title test</span>
-        <div slot="content">
-            <p>Kickstart your development with the Sveltekit Components library, a tool of the Sveltekit ecosystem by TheUI. It includes the power of Sveltekit and TailwindCSS to speed up your development. So, build fast, deliver faster.</p>
-        </div>
+        <p>Kickstart your development with the Sveltekit Components library, a tool of the Sveltekit ecosystem by TheUI. It includes the power of Sveltekit and TailwindCSS to speed up your development. So, build fast, deliver faster.</p>
     </AccordionItem>
     <div class="my-16"></div>
 
     {#each data as d}
-        <AccordionItem data={d} />
+        <AccordionItem title={d.title} content={d.content} />
     {/each}
+
     <div class="my-16"></div>
+
     GROUP
     <Accordion>
         {#each data as d}
-            <AccordionItem data={d} />
+            <AccordionItem title={d.title} content={d.content} />
         {/each}
     </Accordion>
     <div class="my-16"></div>
 
     {#each data as d}
-        <AccordionItem data={d} isFlush={true} />
+        <AccordionItem title={d.title} content={d.content} isFlush={true} />
     {/each}
     <div class="my-16"></div>
     GROUP
     <Accordion>
         {#each data as d}
-            <AccordionItem data={d} isFlush={true} />
+            <AccordionItem title={d.title} content={d.content} isFlush={true} />
         {/each}
     </Accordion>
 </Container>
