f218374c5e2beb9afcd08331b5a0abd34f382e00
mbparvezme <mbparvezme@gmail.com>
2024-11-06 21:14:36 +0600

    regular

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,5 +21,4 @@
       hover:file:bg-violet-100 {roundedClass( "xl", "all", "fileButton" )}"/>
 </div>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
