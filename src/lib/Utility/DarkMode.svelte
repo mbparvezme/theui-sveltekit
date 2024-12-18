3638408484aa6bd3acea00b87cd6be3208ed25b9
M B Parvez (Ron) <mbparvezme@gmail.com>
2024-12-18 12:42:56 +0600

    migration to /state

--- src/lib/Utility/DarkMode.svelte
+++ src/lib/Utility/DarkMode.svelte
@@ -2,7 +2,7 @@
   import { onMount, type Snippet } from "svelte"
   import { twMerge } from "tailwind-merge"
 
-  let {darkMode = true, icon = undefined, ...props} : {darkMode?: boolean | 'system' | 'all', icon?: Snippet|undefined, [key: string]: unknown} = $props()
+  let {darkMode = true, children, ...props} : {darkMode?: boolean | 'system' | 'all', children?: Snippet, [key: string]: unknown} = $props()
   let toggleTheme: () => void = () => {}
 
   onMount((): void => {
@@ -26,8 +26,8 @@
 
 {#if darkMode !== false}
 <button class={"theui-theme-toggler " + twMerge("bg-transparent p-0.5", (props?.class ?? "") as string)} onclick={()=>toggleTheme()} aria-label="Toggle light or dark mode">
-  {#if icon}
-    {@render icon()}
+  {#if children}
+    {@render children()}
   {:else}
     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" class="w-4 h-4">
       <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import "../app.css";
 	import "$lib/style.css";
 	let { children } = $props()
+
+	import { navigating, page, updated } from "$app/state";
 </script>
 
 {@render children()}
+
+{JSON.stringify(page)}
